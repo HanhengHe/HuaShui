cc95e9e03de89449743296ecc6a850dc3dd2d360
Emh <745981022@qq.com>
2019-11-10 23:49:14 +0800

    预处理后的SVR

--- Emh/SVR/svr.py
+++ Emh/SVR/svr.py
@@ -1,3 +1,5 @@
+from random import randint
+
 import numpy as np
 from sklearn.svm import SVR
 import matplotlib.pyplot as plt
@@ -27,31 +29,57 @@ for it in range(4, nRows):
 rate = 0.4
 size = int(rate * (nRows - 4))
 
+# 生成随机数列
+randList = []
+while True:
+    rand = randint(0, len(X)-1)
+    if rand in randList:
+        pass
+    else:
+        randList.append(rand)
+    if len(randList) == size:
+        break
+
+randList.sort()
+
+trainList = []
+trainLabel = []
+testList = []
+testLabel = []
+
+for i in range(len(X)):
+    if i in randList:
+        trainList.append(X[i])
+        trainLabel.append(y[i])
+    else:
+        testList.append(X[i])
+        testLabel.append(y[i])
+
 # 调用模型
 svr_rbf = SVR(C=0.025, epsilon=0.0002, gamma=2,  kernel='rbf', max_iter=500, shrinking=True, tol=0.005, )
 
-svr_rbf.fit(np.mat(X[:size]), y[:size])
-y_rbf = svr_rbf.predict(np.mat(X[size:]))
+svr_rbf.fit(np.mat(trainList), trainLabel)
+y_rbf = svr_rbf.predict(np.mat(testList))
 
 # 可视化结果
 eta0 = 0.05
 eta1 = 0.1
 
 lw = 2
-plt.scatter([i for i in range(len(y[size:]))], y[size:], color='darkorange', label='Real Data')
-plt.scatter([i for i in range(len(y[size:]))], y_rbf, color='navy', lw=lw, label='RBF predict')
+plt.scatter([i for i in range(len(testLabel))], testLabel, color='darkorange', label='Real Data')
+plt.scatter([i for i in range(len(testLabel))], y_rbf, color='navy', lw=lw, label='RBF predict')
 plt.xlabel('number')
 plt.ylabel('COD_Out')
 plt.title('SVR for COD OUT')
 plt.legend()
 plt.show()
 
-error = np.abs(np.array(y[size:])-np.array(y_rbf))
-plt.plot([i for i in range(len(y[size:]))], [eta0]*len(y[size:]), color='navy')
-plt.plot([i for i in range(len(y[size:]))], [eta1]*len(y[size:]), color='navy')
-plt.scatter([i for i in range(len(y[size:]))], error, color='darkorange', lw=lw, label='error')
-perErrorRate = error / np.array(y[size:])
-MeanErrorRate = np.sum(perErrorRate) / len(y[size:])
+error = np.abs(np.array(testLabel)-np.array(y_rbf))
+plt.plot([i for i in range(len(testLabel))], [eta0]*len(testLabel), color='navy')
+plt.plot([i for i in range(len(testLabel))], [eta1]*len(testLabel), color='navy')
+plt.scatter([i for i in range(len(testLabel))], error, color='darkorange', lw=lw, label='error')
+perErrorRate = error / np.array(testLabel)
+MeanErrorRate = np.sum(perErrorRate) / len(testLabel)
 
 counter0 = 0
 for e in error:
@@ -65,7 +93,7 @@ for e in error:
 
 title = 'Mean error rate: ' + (str(MeanErrorRate))[:6] + '\nMax error rate: ' \
         + (str(perErrorRate.max()))[:6] + '\n' + 'Eta0-ErrorRate is: ' \
-        + (str(counter0 / len(y[size:]))[:6])+ '\n' + 'Eta1-ErrorRate is: ' \
-        + (str(counter1 / len(y[size:]))[:6])
+        + (str(counter0 / len(testLabel))[:6]) + '\n' + 'Eta1-ErrorRate is: ' \
+        + (str(counter1 / len(testLabel))[:6])
 plt.title(title)
 plt.show()
