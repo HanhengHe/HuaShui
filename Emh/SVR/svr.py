a6869c7834c9cb90ed0d0171443e3e556e1154c8
Emh <745981022@qq.com>
2019-11-10 20:00:14 +0800

    SVR代码

--- Emh/SVR/svr.py
+++ Emh/SVR/svr.py
@@ -31,10 +31,12 @@ size = int(rate * (nRows - 4))
 svr_rbf = SVR(C=1.0, cache_size=200, coef0=0.0, epsilon=0.2, gamma=0.1,
               kernel='rbf', max_iter=-1, shrinking=True, tol=0.001, verbose=False)
 
-svr_rbf.fit(np.mat(X[:size]), np.mat(y[:size]).T)
+svr_rbf.fit(np.mat(X[:size]), y[:size])
 y_rbf = svr_rbf.predict(np.mat(X[size:]))
 
 # 可视化结果
+eta = 0.05
+
 lw = 2
 plt.scatter([i for i in range(len(y[size:]))], y[size:], color='darkorange', label='Real Data')
 plt.plot([i for i in range(len(y[size:]))], y_rbf, color='navy', lw=lw, label='RBF predict')
@@ -45,8 +47,18 @@ plt.legend()
 plt.show()
 
 error = np.abs(np.array(y[size:])-np.array(y_rbf))
-plt.plot([i for i in range(len(y[size:]))], error)
+plt.plot([i for i in range(len(y[size:]))], [eta]*len(y[size:]), color='darkorange', label='errorAllow')
+plt.plot([i for i in range(len(y[size:]))], error, color='navy', lw=lw, label='error')
 perErrorRate = error/np.array(y[size:])
 errorRate = np.sum(perErrorRate)/len(y[size:])
-plt.title('Mean error rate: ' + str(errorRate)+'\nMax error rate: ' + str(perErrorRate.max()))
+
+counter = 0
+for e in perErrorRate:
+    if e >= eta:
+        counter += 1
+
+etaRate = eta/len(y[size:])
+title = 'Mean error rate: ' + str(errorRate) + '\nMax error rate: ' \
+        + str(perErrorRate.max()) + '\n' + 'Eta-ErrorRate is: ' + str(counter/len(y[size:]))
+plt.title(title)
 plt.show()
