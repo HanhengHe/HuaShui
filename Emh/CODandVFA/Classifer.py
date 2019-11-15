1b9965f6333c3c730838f98030b5f30ea3e9a1f9
EmhRk <43347776+EmhRk@users.noreply.github.com>
2019-11-16 02:09:16 +0800

    尝试对VFA进行分类

--- Emh/CODandVFA/Classifer.py
+++ Emh/CODandVFA/Classifer.py
@@ -3,13 +3,12 @@ import xlrd
 from random import randint
 from sklearn.ensemble import GradientBoostingClassifier as GBC
 from sklearn.svm import SVC
-from xgboost import XGBClassifier as XGBC
 
 
 def classifier(target):
     # 读取数据
 
-    wb = xlrd.open_workbook('./errorCounterFix.xls')
+    wb = xlrd.open_workbook('./newEC.xls')
     ws = wb.sheet_by_index(0)
 
     rX = []
@@ -22,21 +21,38 @@ def classifier(target):
 
     gap = 20  # changeable
 
-    c1 = 0
-    c_1 = 0
+    c = 0
     c0 = 0
 
     # 跳过表头 data: 2:14, 上四分位数：0.1735 下四分位数：0.2378
     for it in range(1, ws.nrows):
         try:
-            rX.append([float(ws.cell_value(it, 2)), float(ws.cell_value(it, 3)), float(ws.cell_value(it, 4)),
-                       float(ws.cell_value(it, 5)), float(ws.cell_value(it, 6)), float(ws.cell_value(it, 7)),
-                       float(ws.cell_value(it, 8)), float(ws.cell_value(it, 9)), float(ws.cell_value(it, 10)),
-                       float(ws.cell_value(it, 11)), float(ws.cell_value(it, 12)), float(ws.cell_value(it, 13)),
-                       float(ws.cell_value(it, 14)),
+            rX.append([float(ws.cell_value(it, 1)), float(ws.cell_value(it, 2)), float(ws.cell_value(it, 3)),
+                       float(ws.cell_value(it, 4)), float(ws.cell_value(it, 5)), float(ws.cell_value(it, 6)),
+                       float(ws.cell_value(it, 7)), float(ws.cell_value(it, 8)), float(ws.cell_value(it, 9)),
+                       float(ws.cell_value(it, 10)), float(ws.cell_value(it, 11)), float(ws.cell_value(it, 12)),
+                       float(ws.cell_value(it, 13)),
                        ])
-            if float(ws.cell_value(it, 1)) >= gap:
-                if float(ws.cell_value(it, 15)) >= 0.2378:
+            if ws.cell_value(it, 1) == 0:
+                X0.append([float(ws.cell_value(it, 1)), float(ws.cell_value(it, 2)), float(ws.cell_value(it, 3)),
+                       float(ws.cell_value(it, 4)), float(ws.cell_value(it, 5)), float(ws.cell_value(it, 6)),
+                       float(ws.cell_value(it, 7)), float(ws.cell_value(it, 8)), float(ws.cell_value(it, 9)),
+                       float(ws.cell_value(it, 10)), float(ws.cell_value(it, 11)), float(ws.cell_value(it, 12)),
+                       float(ws.cell_value(it, 13)),
+                       ])
+                y0.append(0)
+                c0 += 1
+            else:
+                X.append([float(ws.cell_value(it, 1)), float(ws.cell_value(it, 2)), float(ws.cell_value(it, 3)),
+                           float(ws.cell_value(it, 4)), float(ws.cell_value(it, 5)), float(ws.cell_value(it, 6)),
+                           float(ws.cell_value(it, 7)), float(ws.cell_value(it, 8)), float(ws.cell_value(it, 9)),
+                           float(ws.cell_value(it, 10)), float(ws.cell_value(it, 11)), float(ws.cell_value(it, 12)),
+                           float(ws.cell_value(it, 13)),
+                           ])
+                y.append(0)
+                c += 1
+            """if float(ws.cell_value(it, 0)) == 1:
+                if float(ws.cell_value(it, 0)) >= 0.2378:
                     X.append([float(ws.cell_value(it, 2)), float(ws.cell_value(it, 3)), float(ws.cell_value(it, 4)),
                               float(ws.cell_value(it, 5)), float(ws.cell_value(it, 6)), float(ws.cell_value(it, 7)),
                               float(ws.cell_value(it, 8)), float(ws.cell_value(it, 9)),
@@ -80,17 +96,17 @@ def classifier(target):
                            ])
                 c0 += 1
                 y0.append(0)  # 中间值
-                ry.append(0)
+                ry.append(0)"""
         except ValueError:
             pass
-    print('%s, %s, %s' % (c_1, c1, c0))
-    print((c_1 + c1) / (c_1 + c1 + c0))
+    print('%s, %s' % (c, c0))
+    print(c / (c + c0))
 
     #  随机生成(c0+c1)/2个0类插入y
     r0 = []
     while True:
         rand = randint(0, len(y0) - 1)
-        if len(r0) > (c1 + c_1) / 2:
+        if len(r0) > c:
             break
         if rand in r0:
             continue
@@ -127,7 +143,7 @@ def classifier(target):
 
         for i in range(len(X)):
             if y[i] == 0:
-                if y0C > (c1 + c_1) / 2:
+                if y0C > c:
                     continue
             if i in randList:
                 trainList.append(X[i])
--- Emh/CODandVFA/mainScript(WithTimeOrder).py
+++ Emh/CODandVFA/mainScript(WithTimeOrder).py
@@ -4,7 +4,6 @@ import xlwt
 import matplotlib.pyplot as plt
 from random import randint
 
-from Emh.CODandVFA.Classifer import classifier
 from Emh.CODandVFA.gbr4VFA import gbrSearcher
 from Emh.CODandVFA.svr4VFA import svrSearcher
 
@@ -23,9 +22,10 @@ temperatureList = []
 
 # 跳过列名
 # 2 days
-for it in range(1, nRows-1):
+for it in range(1, nRows - 1):
     try:
-        if table.cell_value(it+1, 0)-table.cell_value(it, 0) == 1:  # and table.cell_value(it+2, 0)-table.cell_value(it+1, 0) == 1:
+        if table.cell_value(it + 1, 0) - table.cell_value(it,
+                                                          0) == 1:  # and table.cell_value(it+2, 0)-table.cell_value(it+1, 0) == 1:
             X.append([float(table.cell_value(it, 1)), float(table.cell_value(it, 4)), float(table.cell_value(it, 5)),
                       float(table.cell_value(it, 8)),  # 这一列的影响不明
                       float(table.cell_value(it, 7)), float(table.cell_value(it, 11)),
@@ -38,13 +38,15 @@ for it in range(1, nRows-1):
 
                       float(table.cell_value(it, 13)),  # 昨天的出水VFA
 
-                      float(table.cell_value(it+1, 1)), float(table.cell_value(it+1, 4)), float(table.cell_value(it+1, 5)),
-                      float(table.cell_value(it+1, 8)),  # 这一列的影响不明
-                      float(table.cell_value(it+1, 7)), float(table.cell_value(it+1, 11)),
+                      float(table.cell_value(it + 1, 1)), float(table.cell_value(it + 1, 4)),
+                      float(table.cell_value(it + 1, 5)),
+                      float(table.cell_value(it + 1, 8)),  # 这一列的影响不明
+                      float(table.cell_value(it + 1, 7)), float(table.cell_value(it + 1, 11)),
                       # 入水温度和罐内温度， 我们怀疑这两个个对VFA有一定影响。罐内温度对聚类结果有很明显的影响作用
-                      float(table.cell_value(it+1, 9)),  # 入水VFA
-                      float(table.cell_value(it+1, 10)),  # 罐内pH，理论上这里列该是稳定的， 考虑删除
-                      float(table.cell_value(it+1, 2)), float(table.cell_value(it+1, 3)), float(table.cell_value(it+1, 6)),
+                      float(table.cell_value(it + 1, 9)),  # 入水VFA
+                      float(table.cell_value(it + 1, 10)),  # 罐内pH，理论上这里列该是稳定的， 考虑删除
+                      float(table.cell_value(it + 1, 2)), float(table.cell_value(it + 1, 3)),
+                      float(table.cell_value(it + 1, 6)),
                       # 进水量、进水COD和进水pH， 这行代表系统负荷
                       # float(table.cell_value(it, 12)), float(table.cell_value(it, 14)),  # 出水COD和COD去除率， 我们怀疑COD和VFA有很大关系
 
@@ -69,7 +71,7 @@ for it in range(1, nRows-1):
             if table.cell_value(it, 11) not in temperature:
                 temperature.append(table.cell_value(it, 11))"""
 
-            VFA.append(table.cell_value(it+1, 13) * 0.9 + 0.1)  # [0,1]区间映射到[0.1,1]
+            VFA.append(table.cell_value(it + 1, 13) * 0.9 + 0.1)  # [0,1]区间映射到[0.1,1]
             # VFA.append(table.cell_value(it, 14))  # 这个是COD！
     except ValueError:
         pass
@@ -77,8 +79,8 @@ for it in range(1, nRows-1):
 rate = 0.6
 size = int(rate * (nRows - 1))
 
-errorRecorder005 = [0]*len(X)
-errorRecorder01 = [0]*len(X)
+errorRecorder005 = [0] * len(X)
+errorRecorder01 = [0] * len(X)
 
 eta0 = 0.05
 eta1 = 0.1
@@ -89,7 +91,13 @@ GBRR2Sum = 0
 SVRMAESum = 0
 SVRR2Sum = 0
 
-for index in range(50):
+maeRecorder = [0] * len(X)
+
+gap = 0.3
+
+steps = 5
+
+for index in range(steps):
     # 生成随机数列
     randList = []
     leftList = []
@@ -111,7 +119,6 @@ for index in range(50):
     trainLabel = []
     testList = []
     testLabel = []
-    realLabel = []
 
     for i in range(len(X)):
         if i in randList:
@@ -129,6 +136,9 @@ for index in range(50):
     color = 'red'"""
 
     [MeanErrorRate, y_predict, counter005, counter01, error] = gbrSearcher(trainList, trainLabel, testList, testLabel)
+    for i in range(len(error)):
+        if abs(error[i] - testLabel[i]) >= gap:
+            maeRecorder[leftList[i]] += (error[i] - testLabel[i]) / abs(error[i] - testLabel[i])
     """y_valueU = []
     y_fixU = []
     y_valueD = []
@@ -144,7 +154,9 @@ for index in range(50):
     markerSizeD = markerSizeD*len(y_fixD)"""
     mean = np.mean(testLabel)
     R2 = 1 - np.sum(np.square(error)) / np.sum(np.square(np.array(testLabel) - mean))
-    print('%s,GBR: u 0.05: %s, u 0.1: %s, meanER: %s, R2: %s' % (index+1, str(counter005/len(leftList))[:5], str(counter01/len(leftList))[:5], str(MeanErrorRate)[:5], str(R2)[:5]))
+    print('%s,GBR: u 0.05: %s, u 0.1: %s, meanER: %s, R2: %s' % (
+    index + 1, str(counter005 / len(leftList))[:5], str(counter01 / len(leftList))[:5], str(MeanErrorRate)[:5],
+    str(R2)[:5]))
     # title = 'GBR for VFA out (3 Consecutive Day)\nMAE: ' + (str(MeanErrorRate))[:6] + '; R2: ' + str(R2)[:5]
     # plt.figure(figsize=(18, 12), dpi=300)
     # plt.scatter(y_fixU, y_valueU, s=markerSizeU, color=color, marker='+', label='classifier predict(predict to low)')
@@ -161,8 +173,7 @@ for index in range(50):
     GBRMAESum += MeanErrorRate
     GBRR2Sum += R2
 
-
-    [MeanErrorRate, y_predict, counter005, counter01, error] = svrSearcher(trainList, trainLabel, testList, testLabel)
+    # [MeanErrorRate, y_predict, counter005, counter01, error] = svrSearcher(trainList, trainLabel, testList, testLabel)
     """y_valueU = []
     y_fixU = []
     y_valueD = []
@@ -174,9 +185,9 @@ for index in range(50):
         elif y_fix[i] == -1:
             y_fixD.append(i)
             y_valueD.append(y_predict[i])"""
-    mean = np.mean(testLabel)
-    R2 = 1 - np.sum(np.square(error)) / np.sum(np.square(np.array(testLabel) - mean))
-    print('  SVR: u 0.05: %s, u 0.1: %s, meanER: %s, R2: %s\n' % (str(counter005 / len(leftList))[:5], str(counter01 / len(leftList))[:5], str(MeanErrorRate)[:5], str(R2)[:5]))
+    # mean = np.mean(testLabel)
+    # R2 = 1 - np.sum(np.square(error)) / np.sum(np.square(np.array(testLabel) - mean))
+    # print('  SVR: u 0.05: %s, u 0.1: %s, meanER: %s, R2: %s\n' % (str(counter005 / len(leftList))[:5], str(counter01 / len(leftList))[:5], str(MeanErrorRate)[:5], str(R2)[:5]))
     # title = 'SVR for VFA out (3 Consecutive Day)\nMAE: ' + (str(MeanErrorRate))[:6] + '; R2: ' + str(R2)[:5]
     # plt.figure(figsize=(18, 12), dpi=300)
     # plt.scatter(y_fixU, y_valueU, s=markerSizeU, color=color, marker='+', label='classifier predict(predict to low)')
@@ -190,8 +201,8 @@ for index in range(50):
     # plt.savefig("./Result/3 Consecutive Day SVR4VFA " + str(index) + ".png", dpi=300)
     # plt.show()
 
-    SVRMAESum += MeanErrorRate
-    SVRR2Sum += R2
+    # SVRMAESum += MeanErrorRate
+    # SVRR2Sum += R2
 
     """for i in range(len(error)):
         if error[i] >= 0.05:
@@ -199,13 +210,21 @@ for index in range(50):
         if error[i] >= 0.1:
             errorRecorder01[leftList[i]] += 1"""
 
-print("GBR: ",end='')
-print(GBRMAESum/50,end=' ')
-print(GBRR2Sum/50)
+print(maeRecorder)
 
-print("SVR: ", end='')
-print(SVRMAESum / 50, end=' ')
-print(SVRR2Sum / 50)
+for i in range(len(maeRecorder)):
+    maeRecorder[i] = maeRecorder[i] / steps
+    if maeRecorder[i] <= 0.5:
+        maeRecorder[i] = 0
+    maeRecorder[i] = maeRecorder[i] / abs(maeRecorder[i]) if maeRecorder[i] != 0 else 0
+
+# print("GBR: ", end='')
+# print(GBRMAESum / steps, end=' ')
+# print(GBRR2Sum / steps)
+
+# print("SVR: ", end='')
+# print(SVRMAESum / steps, end=' ')
+# print(SVRR2Sum / steps)
 
 """print(errorRecorder005)
 print(errorRecorder01)"""
@@ -230,3 +249,15 @@ for i in range(1, len(errorRecorder005)):
         ws.write(i, j+2, X[i-1][j])
     ws.write(i, len(X[0])+2, VFA[i-1])
 wb.save('./errorCounterFix.xls')"""
+
+wb = xlwt.Workbook()
+ws = wb.add_sheet('newEC')
+ws.write(0, 0, 'C')
+ws.write(0, 1, 'data')
+ws.write(0, len(X[0]) + 1, 'VFA')
+for i in range(1, len(errorRecorder005)):
+    ws.write(i, 0, maeRecorder[i - 1])
+    for j in range(len(X[0])):
+        ws.write(i, j + 1, X[i - 1][j])
+    ws.write(i, len(X[0]) + 1, VFA[i - 1])
+wb.save('./newEC.xls')
